9e9ef9362e8280a75f51ec5c20fbc45fd2ccec01
Devamakesh <59350409+mrdm-me@users.noreply.github.com>
2021-06-22 17:42:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB:'mongodb://localhost:27017/covid',
+  MONGODB:'mongodb+srv://demo:demo@1234@alienxapp.re5ij.mongodb.net/alienxapp?retryWrites=true&w=majority',
   SECRET_KEY: 'some very secret 1010101 key'
 };
